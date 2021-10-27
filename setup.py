f52071a9092173be7191c8ac99c3bb015938ec2a
vurdolag <48510944+vurdolag@users.noreply.github.com>
2021-10-27 09:27:35 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+"""
+:authors: vurdolag
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2021 vurdolag
+"""
+
+
+version = '1.0.0'
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='py-func-type-checker',
+    version=version,
+
+    author='vurdolag',
+    author_email='vlad_fotal@mail.ru',
+
+    description=(
+        "This is a simple type checker for python functions."
+    ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/vurdolag/py-func-type-checker',
+    download_url=f'https://github.com/vurdolag/py-func-type-checker/archive/v{version}.zip',
+
+    license='Apache License, Version 2.0, see LICENSE file',
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
